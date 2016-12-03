04a4b89fb41174d2ce111220c21ecbdd13fd8cff
idiotjr <vaisakhjnr@gmail.com>
2016-12-03 17:25:42 +0530

    CardView Image Fix an HD Splash image

--- app/src/main/java/com/nitwit/idiot/lebox/Mokee.java
+++ app/src/main/java/com/nitwit/idiot/lebox/Mokee.java
@@ -3,16 +3,22 @@ package com.nitwit.idiot.lebox;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.widget.TextView;
 
 public class Mokee extends AppCompatActivity {
 
     TextView titletxt, txt1, txt2, txt3;
 
+    CardView cimg;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mokee);
+
+        cimg = (CardView)findViewById(R.id.card0);
+        cimg.setPreventCornerOverlap(false);
 
         titletxt = (TextView)findViewById(R.id.text0);
         txt1 = (TextView)findViewById(R.id.text1);
